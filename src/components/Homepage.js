a95d5ec75bbd79103d358587a2a4b9f523b84b36
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-31 21:01:55 +0300

    Homepage styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,51 @@
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
+import '../Navbar.css'
+
 const Homepage = () => {
+    
+    
     return ( 
+        
     <>
         <Navbar/>
+        <div class="homepage">
+        <div class="hero">
+            <h1>Welcome to Ristorante</h1>
+            <p>Discover the best restaurants in the country, reviewed by customers like you</p>
+            <Link to="/signup" className="signup">
+                <span className="gs-show">
+                    <span className="gs-show2">Sign Up</span>
+                </span>   
+            </Link>
+            {/* <p>We are dedicated to helping you discover the best dining experiences in your area. Our platform provides a wealth of information about restaurants in different locations, making it easy for you to find the perfect spot for your next meal.</p> */}
+        </div>
+        {/* <div class="app">
+            <h2> Some of the Featured Restaurants</h2>
+            <div class="restaurant">
+          
+            <button>View Reviews</button>
+            </div>
+            <div class="restaurant">
+            <img src="restaurant2.jpg" alt="Restaurant 2" />
+            <h3>Restaurant 2</h3>
+            <p>Rating: 4.8/5</p>
+            <button>View Reviews</button>
+            </div>
+            <div class="restaurant">
+            <img src="restaurant3.jpg" alt="Restaurant 3" />
+            <h3>Restaurant 3</h3>
+            <p>Rating: 4.2/5</p>
+
+            </div>
+        </div> */}
+        </div>
+        <footer>
+        <div class="container">
+            <p>&copy; 2023 Ristorante</p>
+        </div>
+        </footer>
+
 
     </> 
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,49 @@
+import React from "react";
+import '../Navbar.css'
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
      <div>
+                <div className="header-navigation">
+            <nav className="navigation-display">
+                <div className="showcase">
+                    <NavLink to="/" className="logo-show">
+                        <div className="logo-wrapper">
+                            <h2><i className="fas fa-project-diagram" id="project-logo"></i><span>Ristorante</span></h2>
+                        </div>
+                    </NavLink>
+                    {/* <ul className="items-nav"> */}
+                        {/* <li> <NavLink to="#" className="lists">Abo</NavLink></li>
+                        <li> <NavLink href="#" className="lists">Contact</NavLink></li>
+                        <li> <NavLink href="#" className="lists">Blog</NavLink></li> */}
+                        {/* <li> <NavLink href="#" className="lists">All Restaurants</NavLink></li> */}
+                    {/* </ul> */}
+                    <ul className="items-nav right-side">
+                    <li>
+                            <NavLink to="/restaurants" className="logo-show login1">
+                                <span>All Restaurants</span>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login" className="logo-show login1">
+                                <span>Log In</span>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <div className="get-started">
+                                <NavLink to="/signup" className="signup">
+                                    <span className="gs-show">
+                                        <span className="gs-show2">Get Started</span>
+                                    </span>   
+                                </NavLink>
 
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+       
+      </div>
     </div> 
     );
 }
