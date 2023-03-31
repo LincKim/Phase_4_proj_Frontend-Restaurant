596cab09a37c170b334d6e96381985f6dccdaa5b
Leon simintei <mallfs@icloud.com>
2023-03-31 09:22:54 +0300

    login

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from 'react';
+ // import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginForm from './Login';
+// import Signup from './Signup';
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
+  
+
+
+  return(
+    <div>
+       <div>
+       <LoginForm/>
+       </div>
+        
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("./login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username }),
+    })
+      .then((r) => r.json())
+      .then((user) =>  (user));
+  
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <button type="submit" className="login-button">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
