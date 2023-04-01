ad4db17952f617ea5a0cb9d48f6ba28eb6ef684d
LincKim <lincolnkimutai@gmail.com>
2023-04-01 12:47:08 +0300

    debugging

--- src/components/myrestaurants.js
+++ src/components/myrestaurants.js
@@ -1,50 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './restaurantlist.css';
 
 function Myrestaurantlist() {
 
-    // GET all myrestaurants
-    function handleMyRestaurants(e){
-        e.preventDefault()
+    const [restaurants, setRestaurants] = useState([]);
 
-            fetch("http://127.0.0.1:3000/ristorantes")
+    // GET all myrestaurants
+    function handleMyRestaurants(e) {
+        e.preventDefault();
+        fetch("http://127.0.0.1:3000/ristorantes")
             .then((r) => r.json())
-            .then(res => {
-                console.log(res)
-                res.forEach(data => {
-                    return(
-                        <div className="main">
-                            <div className="card">
-                                <p>{data.name}</p>
-                            </div>
-                        </div>
-                    )
-                });
-            })
-        
+            .then((res) => {
+                setRestaurants(res);
+            });
     }
-    
+     // Fetch location data for the given id
+     function fetchLocation(id) {
+        return fetch(`http://127.0.0.1:3000/locations/${id}`)
+            .then((r) => r.json())
+            .then(res => console.log(res)) 
+    }
 
     // Delete
-    function handleDelete(id){
-        fetch(`/ristorantes/${id}`, {
+    function handleDelete(id) {
+        fetch(`http://127.0.0.1:3000/ristorantes/${id}`, {
             method: "DELETE",
-        }).then()
+        }).then(() => {
+            setRestaurants(restaurants.filter((r) => r.id !== id));
+        });
     }
 
-    return(
+    return (
         <div className="main">
-
             <div className="card">
                 <button onClick={handleMyRestaurants}>My Restaurants</button>
-            
-            {/* <button onClick={() => handleDelete()}>DELETE</button> */}
-
             </div>
-
+            <div className="card-container">
+                {restaurants.map((restaurant) => (
+                    <div key={restaurant.id} className="card">
+                        <p>{restaurant.name}</p>
+                        <p>{restaurant.email}</p>
+                        <p>Location:</p>
+                        <p>{restaurant.location_id && (
+                            fetchLocation(restaurant.location_id)
+                                .then((location) => location.county)
+                                .catch(() => "Unknown Location")
+                        )}</p>
+                        <button onClick={() => handleDelete(restaurant.id)}>
+                            Delete
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-
+    );
 }
 
 export default Myrestaurantlist;
