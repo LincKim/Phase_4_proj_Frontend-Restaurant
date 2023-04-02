71a93d8ac24a0e78832569ac063d327c197f37b1
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-04-02 13:01:18 +0300

    Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
-import '../App.css';
+// import '../App.css';
 import {Routes, Route} from "react-router-dom"
 import Homepage from "./Homepage";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListAllRestaurants from './ListAllRestaurants';
 import LoginForm from './Login';
 import Signup from './Signup';
 
 
 function App() {
- const [currentForm, setCurrentForm] = useState('login')
+//  const [currentForm, setCurrentForm] = useState('login')
 
-  const toggleForm = (formName) =>{
-    setCurrentForm(formName);
-  }
+//   const toggleForm = (formName) =>{
+//     setCurrentForm(formName);
+//   }
   return (
       <>
       <Routes>
           <Route path="/" element={<Homepage/>} />
+          <Route path='/login' element={<LoginForm/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/restaurants' element={<ListAllRestaurants/>}/>
       </Routes>
-      <BrowserRouter>
-      <ListAllRestaurants />
-    </BrowserRouter>
-      <div>
+      {/* <div>
      {
       currentForm === "login" ? <LoginForm onFormSwitch={toggleForm}/> : <Signup onFormSwitch={toggleForm}/>
      }
-    </div>
+    </div> */}
       </>
   )
   
